b4a25611508c4c20bdf26e4c6d258c69558a53f6
JSH95 <you@example.com>
2024-01-12 11:24:42 +0900

    init

---
+++ src/main/java/com/weavus/weavusshopping/WeavusShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.weavus.weavusshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavusShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeavusShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weavus/weavusshopping/WeavusShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weavus.weavusshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavusShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
